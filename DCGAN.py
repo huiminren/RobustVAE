468599b857e2c14643de03f0d7f4edb05f6ca47e
Huimin Ren <ramonarhm07@gmail.com>
2018-12-21 00:23:46 -0500

    modified znxlwm's code

--- DCGAN.py
+++ DCGAN.py
@@ -27,7 +27,7 @@ def batches(l, n):
         yield range(i,min(l,i+n))
         
 class DCGAN(object):
-    def __init__(self, sess, noise_dim = 100, input_dim = 28, dec_in_channels = 1,learning_rate = 1e-3):
+    def __init__(self, sess, noise_dim = 100, learning_rate = 1e-3, batch_size = 64):
         """
         sess: tf.Session()
         noise_dim: noise dimension (like dimension in latent layer)
@@ -37,9 +37,8 @@ class DCGAN(object):
         """
         
         self.noise_dim = noise_dim
-        self.input_dim = input_dim
-        self.dec_in_channels = dec_in_channels
         self.learning_rate = learning_rate
+        self.batch_size = batch_size
         
         self.build()
         
@@ -47,91 +46,94 @@ class DCGAN(object):
         self.sess.run(tf.global_variables_initializer())
     
     def build(self):
-        # Build Networks
-        # Network Inputs
-        self.noise_input = tf.placeholder(tf.float32, shape=[None, 1, 1, self.noise_dim])
-        self.real_image_input = tf.placeholder(tf.float32, shape=[None, self.input_dim, 
-                                                                  self.input_dim, self.dec_in_channels])
+        # variables : input
+        self.x = tf.placeholder(tf.float32, shape=(None, 64, 64, 1))
+        self.z = tf.placeholder(tf.float32, shape=(None, 1, 1, 100))
+        self.isTrain = tf.placeholder(dtype=tf.bool)
         
-        # Build Generator Network
-        self.gen_sample = self.generator(self.noise_input)
+        # networks : generator
+        G_z = self.generator(self.z, self.isTrain)
         
-        # Build 2 Discriminator Networks (one from noise input, one from generated samples)
-        self.disc_real = self.discriminator(self.real_image_input)
-        self.disc_fake = self.discriminator(self.gen_sample, reuse=True)
-        self.disc_concat = tf.concat([self.disc_real, self.disc_fake], axis=0)
+        # networks : discriminator
+        D_real, D_real_logits = self.discriminator(self.x, self.isTrain)
+        D_fake, D_fake_logits = self.discriminator(G_z, self.isTrain, reuse=True)
         
-        # Build the stacked generator/discriminator
-        self.stacked_gan = self.discriminator(self.gen_sample, reuse=True)
+        # loss for each network
+        D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, 
+                                                                             labels=tf.ones([self.batch_size, 1, 1, 1])))
+        D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, 
+                                                                             labels=tf.zeros([self.batch_size, 1, 1, 1])))
+        self.D_loss = D_loss_real + D_loss_fake
+        self.G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, 
+                                                                             labels=tf.ones([self.batch_size, 1, 1, 1])))
         
-        # Build Targets (real or fake images)
-        self.disc_target = tf.placeholder(tf.int32, shape=[None])
-        self.gen_target = tf.placeholder(tf.int32, shape=[None])
+        # trainable variables for each network
+        T_vars = tf.trainable_variables()
+        D_vars = [var for var in T_vars if var.name.startswith('discriminator')]
+        G_vars = [var for var in T_vars if var.name.startswith('generator')]
         
-        # Build Loss
-        self.disc_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=self.disc_concat, labels=self.disc_target))
-        self.gen_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=self.stacked_gan, labels=self.gen_target))
-        
-        # Build Optimizers
-        self.optimizer_gen = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-        self.optimizer_disc = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-
-        # Training Variables for each optimizer
-        # By default in TensorFlow, all variables are updated by each optimizer, so we
-        # need to precise for each one of them the specific variables to update.
-        # Generator Network Variables
-        self.gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
-        # Discriminator Network Variables
-        self.disc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Discriminator')
-        
-        # Create training operations
-        self.train_gen = self.optimizer_gen.minimize(self.gen_loss, var_list=self.gen_vars)
-        self.train_disc = self.optimizer_disc.minimize(self.disc_loss, var_list=self.disc_vars)
+        # optimizer for each network
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            self.D_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=0.5).minimize(self.D_loss, var_list=D_vars)
+            self.G_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=0.5).minimize(self.G_loss, var_list=G_vars)
         
 
     def lrelu(self, x, alpha=0.3):
         return tf.maximum(x, tf.multiply(x, alpha))
     
-    # Generator Network
-    def generator(self, X_in, reuse = False):
-        """ 
-        Input: X_in: Noise; reuse: Boolean 
-        Output: Image
-        """
-        activation = self.lrelu
-        with tf.variable_scopre('generator',reuse = reuse):
-            x = tf.layers.conv2d(X_in, filters=1024, kernel_size=4, strides=1, padding='valid', activation=activation)
-            x = tf.layers.conv2d(x, filters=512, kernel_size=4, strides=2, padding='same', activation=activation)
-            x = tf.layers.conv2d(x, filters=256, kernel_size=4, strides=2, padding='same', activation=activation)
-            x = tf.layers.conv2d(x, filters=128, kernel_size=4, strides=2, padding='same', activation=activation)
-            x = tf.layers.conv2d(x, filters=1, kernel_size=4, strides=2, padding='same', activation=activation)
-            # Apply sigmoid to clip values between 0 and 1
-            output = tf.nn.sigmoid(x)
-
-        return output
-    
-    # Discriminator Network
-    def discriminator(self, X_in, reuse = False):
-        """ 
-        Input: X_in: Image; reuse: boolean 
-        Output: Prediction Real/Fake Image
-        """
-        with tf.variable_scopre('discriminator',reuse = reuse):
-            x = tf.layers.conv2d(X_in,filters=128, kernel_size=4, strides=2, padding='same', activation=self.lrelu)
-            x = tf.layers.conv2d(x,filters=256, kernel_size=4, strides=2, padding='same', activation=self.lrelu)
-            x = tf.layers.conv2d(x,filters=512, kernel_size=4, strides=2, padding='same', activation=self.lrelu)
-            x = tf.layers.conv2d(x,filters=1024, kernel_size=4, strides=2, padding='same', activation=self.lrelu)
-            x = tf.layers.conv2d(x,filters=1, kernel_size=4, strides=1, padding='valid', activation=self.lrelu)
-            x = tf.nn.tanh(x)
-            # Output 2 classes: Real and Fake images
-            output = tf.layers.dense(x,2)
+    # G(z)
+    def generator(self, x, isTrain=True, reuse=False):
+        with tf.variable_scope('generator', reuse=reuse):
         
-        return output
+            # 1st hidden layer
+            conv1 = tf.layers.conv2d_transpose(x, 1024, [4, 4], strides=(1, 1), padding='valid')
+            lrelu1 = self.lrelu(tf.layers.batch_normalization(conv1, training=isTrain), 0.2)
+        
+            # 2nd hidden layer
+            conv2 = tf.layers.conv2d_transpose(lrelu1, 512, [4, 4], strides=(2, 2), padding='same')
+            lrelu2 = self.lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+        
+            # 3rd hidden layer
+            conv3 = tf.layers.conv2d_transpose(lrelu2, 256, [4, 4], strides=(2, 2), padding='same')
+            lrelu3 = self.lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+        
+            # 4th hidden layer
+            conv4 = tf.layers.conv2d_transpose(lrelu3, 128, [4, 4], strides=(2, 2), padding='same')
+            lrelu4 = self.lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
+        
+            # output layer
+            conv5 = tf.layers.conv2d_transpose(lrelu4, 1, [4, 4], strides=(2, 2), padding='same')
+            o = tf.nn.tanh(conv5)
+        
+            return o
+        
+    # D(x)
+    def discriminator(self, x, isTrain=True, reuse=False):
+        with tf.variable_scope('discriminator', reuse=reuse):
+            # 1st hidden layer
+            conv1 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding='same')
+            lrelu1 = self.lrelu(conv1, 0.2)
+        
+            # 2nd hidden layer
+            conv2 = tf.layers.conv2d(lrelu1, 256, [4, 4], strides=(2, 2), padding='same')
+            lrelu2 = self.lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+        
+            # 3rd hidden layer
+            conv3 = tf.layers.conv2d(lrelu2, 512, [4, 4], strides=(2, 2), padding='same')
+            lrelu3 = self.lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+        
+            # 4th hidden layer
+            conv4 = tf.layers.conv2d(lrelu3, 1024, [4, 4], strides=(2, 2), padding='same')
+            lrelu4 = self.lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
+        
+            # output layer
+            conv5 = tf.layers.conv2d(lrelu4, 1, [4, 4], strides=(1, 1), padding='valid')
+            o = tf.nn.sigmoid(conv5)
+        
+            return o, conv5
+
     
-    
-    def fit(self, X_in, path, num_gen = 100, num_epoch =100, batch_size = 64):
+    def fit(self, X_in, path, num_gen = 100, num_epoch =100):
         """
         X_in: image without label
         """
@@ -139,29 +141,26 @@ class DCGAN(object):
         gl_loss = []
         dl_loss = []
         
-        # Prepare Targets (Real image: 1, Fake image: 0)
-        # The first half of data fed to the generator are real images,
-        # the other half are fake images (coming from the generator).
-        batch_disc_y = np.concatenate(
-            [np.ones([batch_size]), np.zeros([batch_size])], axis=0)
-        # Generator tries to fool the discriminator, thus targets are 1.
-        batch_gen_y = np.ones([batch_size])
-        
         for epoch in range(num_epoch):
-            for one_batch in batches(sample_size, batch_size):
-                # Generate noise to feed to the generator
-                z = np.random.uniform(-1., 1., size=[batch_size, 1, 1, self.noise_dim])
-                ######## why set inside of batch ????
-                # Training
-                feed_dict = {self.real_image_input: X_in[one_batch], self.noise_input: z,
-                             self.disc_target: batch_disc_y, self.gen_target: batch_gen_y}
-                _, _, gl, dl = self.sess.run([self.train_gen, self.train_disc, self.gen_loss, self.disc_loss],
-                                feed_dict=feed_dict)
-            gl_loss.append(gl)
-            dl_loss.append(dl)
+            G_losses = []
+            D_losses = []
+            for one_batch in batches(sample_size, self.batch_size):
+                # update discriminator
+                z_ = np.random.normal(0, 1, (self.batch_size, 1, 1, self.noise_dim))
+                loss_d_, _ = self.sess.run([self.D_loss, self.D_optim], {self.x: X_in[one_batch], self.z: z_, self.isTrain: True})
+                D_losses.append(loss_d_)
+        
+                # update generator
+                z_ = np.random.normal(0, 1, (self.batch_size, 1, 1, self.noise_dim))
+                loss_g_, _ = self.sess.run([self.G_loss, self.G_optim], {self.z: z_, self.x: X_in[one_batch], self.isTrain: True})
+                G_losses.append(loss_g_)
+                
             if epoch % 1 == 0:
-                print('Step %i: Generator Loss: %f, Discriminator Loss: %f' % (epoch, gl, dl))
+                print('Step %i: Generator Loss: %f, Discriminator Loss: %f' % (epoch, np.mean(G_losses), np.mean(D_losses)))
                 self.plot(path = path, fig_name = "generator_"+str(epoch)+".png", num_gen = num_gen)
+            
+            gl_loss.append(np.mean(G_losses))
+            dl_loss.append(np.mean(D_losses))
                 
         np.save(path+"gl_loss.npy",np.array(gl_loss))
         np.save(path+"dl_loss.npy",np.array(dl_loss))
@@ -170,7 +169,7 @@ class DCGAN(object):
         
         np.random.seed(595)
         h = w = 28
-        z = np.random.uniform(-1., 1., size=[num_gen, 1, 1, self.noise_dim])
+        z = np.random.normal(0, 1, (num_gen, 1, 1, self.noise_dim))
         g = self.get_generation(z)
         
         # plot of generation
@@ -186,15 +185,20 @@ class DCGAN(object):
         plt.show()
         
     def get_generation(self, z):
-        return self.sess.run(self.gen_sample, feed_dict={self.noise_input: z})
+        return self.sess.run(self.G_z, feed_dict={self.z: z, self.isTrain: False})
     
 
 def main(noise_factors,debug = True):
     start_time = time.time()
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-    x_train = mnist.train.images
-    input_dim = 28
-    x_train = x_train.reshape(-1,input_dim,input_dim)
+    
+    tf.reset_default_graph()
+    # open session and initialize all variables
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+    
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True, reshape=[])
+    x_train = tf.image.resize_images(mnist.train.images, [64, 64]).eval()
+    x_train = (x_train - 0.5) / 0.5  # normalization; range: -1 ~ 1
     
     batch_size = 256
     num_epoch = 30
@@ -216,12 +220,10 @@ def main(noise_factors,debug = True):
         x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
         x_train_noisy = np.clip(x_train_noisy, 0., 1.)
         
-        tf.reset_default_graph()
-        sess = tf.Session()
-        dcgan = DCGAN(sess, noise_dim = 100, input_dim = input_dim, dec_in_channels = 1,learning_rate = 1e-3)
-        dcgan.fit(x_train_noisy,path = path, num_gen = num_gen, num_epoch = num_epoch, batch_size = batch_size)
-        sess.close()
-    
+        
+        dcgan = DCGAN(sess, noise_dim = 100, learning_rate = 1e-3, batch_size = batch_size)
+        dcgan.fit(x_train_noisy,path = path, num_gen = num_gen, num_epoch = num_epoch)
+    sess.close()
     print("running time: ",time.time()-start_time)
     
 if __name__ == "__main__":  
