9e998bca9ab0a773b970477d822d7b36c4522358
YunYunY <yueylink@gmail.com>
2019-01-05 14:45:17 -0500

    FID tested and GAN tested

--- GAN_basic.py
+++ GAN_basic.py
@@ -6,10 +6,9 @@ Reference:
     https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/gan.py
 
 """
-# TODO all data should be scaled in all model
 # TODO double check nois_dim
 # TODO discuss total loss
-# TODO discuss plot
+# TODO discuss plot1
 # TODO huimin double check plot reshape to 64, 64
 
 
@@ -24,6 +23,10 @@ import time
 import os
 
 
+def batches(l, n):
+    """Yield successive n-sized batches from l, the last batch is the left indexes."""
+    for i in range(0, l, n):
+        yield range(i, min(l, i + n))
 
 
 class GAN_basic(object):
@@ -92,13 +95,14 @@ class GAN_basic(object):
         # Network Inputs
         self.z = tf.placeholder(tf.float32, shape=[None, self.noise_dim], name='input_noise')
         self.x = tf.placeholder(tf.float32, shape=[None, 784], name='disc_input')
+        self.isTrain = tf.placeholder(dtype=tf.bool)
 
         # Build Generator Network
-        self.G_z = self.generator(self.z)
+        self.G_z = self.generator(self.z, self.isTrain)
 
         # Build 2 Discriminator Networks (one from noise input, one from generated samples)
-        disc_real = self.discriminator(self.x)
-        disc_fake = self.discriminator(self.G_z)
+        disc_real = self.discriminator(self.x, self.isTrain)
+        disc_fake = self.discriminator(self.G_z, self.isTrain)
 
         # Build Loss
         self.G_loss = -tf.reduce_mean(tf.log(disc_fake))
@@ -108,53 +112,53 @@ class GAN_basic(object):
         self.D_optim = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.D_loss)
 
     # G(z)
-    def generator(self, x):
+    def generator(self, x, isTrain=True):
         with tf.variable_scope('generator', reuse=tf.AUTO_REUSE):
 
             hidden_layer1 = tf.matmul(x, self.weights['gen_hidden1'])
             hidden_layer1 = tf.add(hidden_layer1, self.biases['gen_hidden1'])
-            hidden_layer1 = tf.nn.sigmoid(hidden_layer1)
+            hidden_layer1 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer1, training=isTrain))
 
             hidden_layer2 = tf.matmul(hidden_layer1, self.weights['gen_hidden2'])
             hidden_layer2 = tf.add(hidden_layer2, self.biases['gen_hidden2'])
-            hidden_layer2 = tf.nn.sigmoid(hidden_layer2)
+            hidden_layer2 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer2, training=isTrain))
 
             hidden_layer3 = tf.matmul(hidden_layer2, self.weights['gen_hidden3'])
             hidden_layer3 = tf.add(hidden_layer3, self.biases['gen_hidden3'])
-            hidden_layer3 = tf.nn.sigmoid(hidden_layer3)
+            hidden_layer3 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer3, training=isTrain))
 
             out_layer = tf.matmul(hidden_layer3, self.weights['gen_out'])
             out_layer = tf.add(out_layer, self.biases['gen_out'])
-            out_layer = tf.nn.sigmoid(out_layer)
+            out_layer = tf.nn.sigmoid(tf.layers.batch_normalization(out_layer, training=isTrain))
             return out_layer
 
 
 
     # D(x)
-    def discriminator(self, x):
+    def discriminator(self, x, isTrain=True):
         with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
             hidden_layer1 = tf.matmul(x, self.weights['disc_hidden1'])
             hidden_layer1 = tf.add(hidden_layer1, self.biases['disc_hidden1'])
-            hidden_layer1 = tf.nn.sigmoid(hidden_layer1)
+            hidden_layer1 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer1, training=isTrain))
 
             hidden_layer2 = tf.matmul(hidden_layer1, self.weights['disc_hidden2'])
             hidden_layer2 = tf.add(hidden_layer2, self.biases['disc_hidden2'])
-            hidden_layer2 = tf.nn.sigmoid(hidden_layer2)
+            hidden_layer2 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer2, training=isTrain))
 
             hidden_layer3 = tf.matmul(hidden_layer2, self.weights['disc_hidden3'])
             hidden_layer3 = tf.add(hidden_layer3, self.biases['disc_hidden3'])
-            hidden_layer3 = tf.nn.sigmoid(hidden_layer3)
+            hidden_layer3 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer3, training=isTrain))
 
             hidden_layer4 = tf.matmul(hidden_layer3, self.weights['disc_hidden4'])
             hidden_layer4 = tf.add(hidden_layer4, self.biases['disc_hidden4'])
-            hidden_layer4 = tf.nn.sigmoid(hidden_layer4)
+            hidden_layer4 = tf.nn.sigmoid(tf.layers.batch_normalization(hidden_layer4, training=isTrain))
 
             out_layer = tf.matmul(hidden_layer4, self.weights['disc_out'])
             out_layer = tf.add(out_layer, self.biases['disc_out'])
             out_layer = tf.nn.sigmoid(out_layer)
             return out_layer
 
-
+    '''
     def fit(self, X_in, path, num_gen=100, num_epoch=30):
         """
         X_in: image without label
@@ -173,12 +177,49 @@ class GAN_basic(object):
 
                 z_ = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
 
-                loss_d_, _ = self.sess.run([self.D_loss, self.D_optim], {self.x: x_, self.z: z_})
+                loss_d_, _ = self.sess.run([self.D_loss, self.D_optim], {self.x: x_, self.z: z_, self.isTrain: True})
                 D_losses.append(loss_d_)
 
                 # update generator
                 z_ = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
                 loss_g_, _ = self.sess.run([self.G_loss, self.G_optim], {self.z: z_})
+                G_losses.append(loss_g_)
+
+            if epoch % 1 == 0:
+                print('Step %i: Generator Loss: %f, Discriminator Loss: %f' % (
+                epoch, np.mean(G_losses), np.mean(D_losses)))
+                self.plot(path=path, fig_name="generator_" + str(epoch) + ".png", num_gen=num_gen)
+
+            gl_loss.append(np.mean(G_losses))
+            dl_loss.append(np.mean(D_losses))
+
+        np.save(path + "gl_loss.npy", np.array(gl_loss))
+        np.save(path + "dl_loss.npy", np.array(dl_loss))
+    '''
+
+
+    def fit(self, X_in, path, num_gen=100, num_epoch=100):
+        """
+        X_in: image without label
+        """
+        sample_size = X_in.shape[0]
+        gl_loss = []
+        dl_loss = []
+
+        for epoch in range(num_epoch):
+            G_losses = []
+            D_losses = []
+            for one_batch in batches(sample_size, self.batch_size):
+                # update discriminator
+                z_ = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
+                loss_d_, _ = self.sess.run([self.D_loss, self.D_optim], {self.x: X_in[one_batch],
+                                                                         self.z: z_, self.isTrain: True})
+                D_losses.append(loss_d_)
+
+                # update generator
+                z_ = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
+                loss_g_, _ = self.sess.run([self.G_loss, self.G_optim],
+                                           {self.z: z_, self.x: X_in[one_batch], self.isTrain: True})
                 G_losses.append(loss_g_)
 
             if epoch % 1 == 0:
@@ -209,10 +250,10 @@ class GAN_basic(object):
         plt.figure(figsize=(8, 8))
         plt.imshow(I_generated, cmap='gray')
         plt.savefig(path + fig_name)
-        plt.show()
+        # plt.show()
 
     def get_generation(self, z):
-        return self.sess.run(self.G_z, feed_dict={self.z: z})
+        return self.sess.run(self.G_z, feed_dict={self.z: z, self.isTrain: False})
 
 
 def main(noise_factors, debug=True):
@@ -225,7 +266,8 @@ def main(noise_factors, debug=True):
     tf.global_variables_initializer().run()
 
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True, reshape=[])
-    x_train = (mnist.train.images - 0.5) / 0.5  # normalization; range: -1 ~ 1
+    # x_train = (mnist.train.images - 0.5) / 0.5  # normalization; range: -1 ~ 1
+    x_train = mnist.train.images
 
     hidden_dim = 5
 
@@ -250,12 +292,13 @@ def main(noise_factors, debug=True):
         x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
         x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 
-        dcgan = GAN_basic(sess, noise_dim=hidden_dim, learning_rate=1e-3, batch_size=batch_size)
-        dcgan.fit(x_train_noisy, path=path, num_gen=num_gen, num_epoch=num_epoch)
+        gan = GAN_basic(sess, noise_dim=hidden_dim, learning_rate=1e-3, batch_size=batch_size)
+        x_train_noisy.shape = (x_train_noisy.shape[0], 784)
+        gan.fit(x_train_noisy, path=path, num_gen=num_gen, num_epoch=num_epoch)
     sess.close()
     print("running time: ", time.time() - start_time)
 
 
 if __name__ == "__main__":
     noise_factors = np.array([0.2, 0.4])
-    main(noise_factors=noise_factors, debug=True)
+    main(noise_factors=noise_factors, debug=False)
