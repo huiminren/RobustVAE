bbff0ad026c11e8036a928a4ca0963ecfbe91716
YunYunY <yueylink@gmail.com>
2019-01-11 21:17:17 -0500

    bk

--- FID/TTUR/precalc_stats_example.py
+++ FID/TTUR/precalc_stats_example.py
@@ -7,12 +7,15 @@ import numpy as np
 import fid
 from scipy.misc import imread
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
 ########
 # PATHS
 ########
-data_path = 'data' # set path to training set images
-output_path = 'fid_stats.npz' # path for where to store the statistics
+data_path = './generation_fid.npy'
+#data_path = '../../MNIST_data/' # set path to training set images
+output_path = 'fid_debug'
+#output_path = 'fid_stats_MNIST.npz' # path for where to store the statistics
 # if you have downloaded and extracted
 #   http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
 # set this path to the directory where the extracted files are, otherwise
@@ -24,8 +27,20 @@ print("ok")
 
 # loads all images into memory (this might require a lot of RAM!)
 print("load images..", end=" " , flush=True)
+'''
 image_list = glob.glob(os.path.join(data_path, '*.jpg'))
 images = np.array([imread(str(fn)).astype(np.float32) for fn in image_list])
+'''
+'''
+mnist = input_data.read_data_sets(data_path, one_hot=True)
+images = mnist.train.images
+'''
+images = np.load(data_path)
+images = np.reshape(images, [-1, 28, 28])
+images = images[0:10000]
+images = np.stack((images,)*3, axis=-1)
+
+
 print("%d images found and loaded" % len(images))
 
 print("create inception graph..", end=" ", flush=True)
